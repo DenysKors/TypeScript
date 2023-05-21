d46ce25d1de384b2ebe61cf71ecdca5b498c8f74
DenysKors <105918404+DenysKors@users.noreply.github.com>
2023-05-21 16:08:59 +0300

    Variable types

--- app.js
+++ app.js
@@ -7,3 +7,42 @@ function sumNumbers(num1, num2) {
 button.addEventListener("click", function () {
     console.log(sumNumbers(Number(input1.value), Number(input2.value)));
 });
+var age;
+var userName;
+var toggle;
+var empty;
+var notInitialize;
+var callback;
+var anything;
+var some;
+some = "Text";
+var str;
+if (typeof some === "string") {
+    str = some;
+}
+var person;
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+var page = {
+    load: Status.READY,
+};
+if (page.load === Status.LOADING) {
+    console.log("Страница загружается");
+}
+if (page.load === Status.READY) {
+    console.log("Страница загружена");
+}
+var param;
+var specParam;
+function showMessaage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
+    return num1 + num2;
+}
+function customError() {
+    throw new Error("Error");
+}
--- app.ts
+++ app.ts
@@ -9,3 +9,59 @@ function sumNumbers(num1: number, num2: number) {
 button.addEventListener("click", function () {
 	console.log(sumNumbers(Number(input1.value), Number(input2.value)));
 });
+
+let age: number;
+let userName: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
+let callback: (a: number) => number;
+
+let anything: any;
+
+let some: unknown;
+some = "Text";
+let str: string;
+if (typeof some === "string") {
+	str = some;
+}
+
+let person: [string, number];
+
+enum Status {
+	LOADING,
+	READY,
+}
+const page = {
+	load: Status.READY,
+};
+
+if (page.load === Status.LOADING) {
+	console.log("Страница загружается");
+}
+if (page.load === Status.READY) {
+	console.log("Страница загружена");
+}
+
+let param: string | number;
+let specParam: "enable" | "disable";
+
+function showMessaage(message: string): void {
+	console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
+	return num1 + num2;
+}
+
+function customError(): never {
+	throw new Error("Error");
+}
+
+type CustomPage = {
+	title: string;
+	likes: number;
+	accounts: string[];
+	status: "open" | "close";
+	details?: { createAt: Date; updateAt: Date };
+};
